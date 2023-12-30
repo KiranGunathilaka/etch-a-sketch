a2d6c9e1e0d45c7b094326c340d39325240c829c
Endeavor360 <kirangunathilaka@gmail.com>
2023-12-31 00:36:48 +0530

    added some buttons

--- script.js
+++ script.js
@@ -1,29 +1,31 @@
-const container = document.querySelector(".container");
+const gridContainer = document.querySelector(".grid-container");
 
 let isMouseDown = false;
 let currentPixel;
 
+function colorChanger(e) {
+    currentPixel = e.target;
+    currentPixel.style.backgroundColor = "black";
+}
+
 for (let i=0; i<1600 ; i++){
     const  pixel = document.createElement("div");
     pixel.classList.add("pixel")
-    container.appendChild(pixel);
+    gridContainer.appendChild(pixel);
 }
 
-container.addEventListener("mousedown", (e) => {
+gridContainer.addEventListener("mousedown", (e) => {
     isMouseDown = true;
-    currentPixel = e.target;
-    currentPixel.style.backgroundColor = "black";
+    colorChanger(e);
 })
 
-container.addEventListener("mousemove", (e) => {
+gridContainer.addEventListener("mousemove", (e) => {
     if(isMouseDown){
-        currentPixel = e.target;
-        currentPixel.style.backgroundColor = "black";
+        colorChanger(e);
     }
 })
 
-container.addEventListener("mouseup", (e) =>{
+gridContainer.addEventListener("mouseup", (e) =>{
     isMouseDown = false;
-    currentPixel = e.target;
-    currentPixel.style.backgroundColor = "black";
+    colorChanger(e);
 })
